3e0f7ea348c6615fc92f7367ba6ff5154b80978b
vituhonorato <vituhonorato.89@gmail.com>
2023-09-22 17:48:20 -0300

    add new answers in node

---
+++ utils/quizzDataNode.ts
@@ -0,0 +1,112 @@
+const quizDataNode = [
+    {
+        question: "Quando uma função JavaScript é invocada (chamada) no Node, onde um novo quadro é colocado?",
+        options: [
+          { text: "call stack", id: "a" },
+          { text: "event loop", id: "b" },
+          { text: "poll phase", id: "c" },
+          { text: "events queue", id: "d" },
+        ],
+        correct: "a",
+      },
+      {
+        question: "Qual dos seguintes é um módulo principal do Node?",
+        options: [
+          { text: "webpack", id: "a" },
+          { text: "crypto", id: "b" },
+          { text: "request", id: "c" },
+          { text: "chalk", id: "d" },
+        ],
+        correct: "b",
+      },
+      {
+        question: "Qual dos seguintes métodos da classe Buffer retorna um buffer não inicializado?",
+        options: [
+          { text: "allocUnsafe", id: "a" },
+          { text: "concat", id: "b" },
+          { text: "from", id: "c" },
+          { text: "alloc", id: "d" },
+        ],
+        correct: "a",
+      },
+      {
+        question: "Qual dos seguintes módulos NÃO é um módulo integrado no Node?",
+        options: [
+          { text: "ftp", id: "a" },
+          { text: "events", id: "b" },
+          { text: "dgram", id: "c" },
+          { text: "http2", id: "d" },
+        ],
+        correct: "a",
+      },
+      {
+        question: "Qual método do módulo fs pode ser usado para ler o conteúdo de um arquivo sem armazená-lo em buffer na memória?",
+        options: [
+          { text: "read", id: "a" },
+          { text: "readFile", id: "b" },
+          { text: "createReadStream", id: "c" },
+          { text: "readFileSync", id: "d" },
+        ],
+        correct: "c",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      {
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },
+      /*{
+        question: "",
+        options: [
+          { text: "", id: "a" },
+          { text: "", id: "b" },
+          { text: "", id: "c" },
+          { text: "", id: "d" },
+        ],
+        correct: "",
+      },*/
+]

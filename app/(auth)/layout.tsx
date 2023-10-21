c08a0c61d107165daa4fec4ad78449e18350585a
vituhonorato <vituhonorato.89@gmail.com>
2023-10-21 01:28:51 -0300

    add a new layout in auth folder

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Tech Quizz',
+    description: 'A tech quizz app to developers',
+  }
+
+
+  export default function RootLayout({
+      children
+  }:{
+    children: React.ReactNode
+  }) {
+    return 
+    <>
+    </>
+  }

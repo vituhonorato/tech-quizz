af9fa0d9d86fb7ffae6fa3ab3302ee0ff1ad91da
vituhonorato <vituhonorato.89@gmail.com>
2023-10-21 01:35:08 -0300

    add clark next in the project

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,25 @@
-import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 export const metadata: Metadata = {
-    title: 'Tech Quizz',
-    description: 'A tech quizz app to developers',
-  }
+  title: 'Tech Quizz',
+  description: 'A tech quizz app to developers',
+};
 
+const inter = Inter({ subsets: ['latin'] });
 
-  export default function RootLayout({
-      children
-  }:{
-    children: React.ReactNode
-  }) {
-    return 
-    <>
-    </>
-  }
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return;
+  <>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}></body>
+      </html>
+    </ClerkProvider>
+  </>;
+}
